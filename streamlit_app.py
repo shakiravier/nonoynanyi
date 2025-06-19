63e95daca8666ce8740a4557da2e15615836acc6
shakiravier <adindapermta@gmail.com>
2025-06-19 22:03:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "nyanyi bukanlah sekedar hobi")
+
 st.header("nonoy")
 st.image("IMG-20250619-WA0100.jpg", width=200)

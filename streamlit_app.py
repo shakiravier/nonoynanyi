fbdd39a7c8c57204dd4bc580f25b0a5028a1596d
shakiravier <adindapermta@gmail.com>
2025-06-19 22:00:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.header("nonoy")
-st.image("IMG-20250619-WA0100.jpg")
+st.image("IMG-20250619-WA0100.jpg", width=200)
